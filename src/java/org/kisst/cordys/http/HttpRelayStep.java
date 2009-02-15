3a9eeda6ff20d1d91683ee5a37b935cda438b5a9
Mark Hooijkaas <mark-git@hooijkaas.net>
2009-02-15 16:15:45 +0100

    code cleanup: moved reusable code to SoapUtil

--- src/java/org/kisst/cordys/http/HttpRelayStep.java
+++ src/java/org/kisst/cordys/http/HttpRelayStep.java
@@ -10,10 +10,6 @@ import com.eibus.xml.nom.Node;
 import com.eibus.xml.nom.XMLException;
 
 public class HttpRelayStep extends HttpBaseStep implements Step {
-
-	private static final String wsaNamespace="http://www.w3.org/2005/08/addressing";
-	//private static final String wsaAnonymous="http://www.w3.org/2005/08/addressing/anonymous";
-	
 	private final boolean wsa;
 	
 	public HttpRelayStep(CompilationContext compiler, final int node) {
@@ -22,7 +18,7 @@ public class HttpRelayStep extends HttpBaseStep implements Step {
 	}
 	
 	public void executeStep(final ExecutionContext context) {
-		int bodyNode= body.getNode(context);
+	    int bodyNode= body.getNode(context);
 	    int httpResponse = 0;
 	    try {
 		    if (wsa) {
@@ -32,32 +28,10 @@ public class HttpRelayStep extends HttpBaseStep implements Step {
 	    	String xml=Node.writeToString(bodyNode, prettyPrint);
 		    		
 	    	byte[] responseBytes=call(context, xml);
-	    	// This code merges the response from the HTTP call with the
-	    	// boilerplate response that Cordys made. The boilerplate response
-	    	// Body should be cleared, but the Header has some important Cordys info.
-	    	// The merging is a bit tricky, because of namespace prefix differences.
 	    	httpResponse = context.getDocument().load(responseBytes);
 			int cordysResponse=context.getXmlVar("output");
-			cordysResponse=Node.getParent(cordysResponse); // get Soap:Body
-			cordysResponse=Node.getParent(cordysResponse); // get Soap:Envelope
-			
-			// copy Envelope attributes
-			NomUtil.copyAttributes(httpResponse, cordysResponse);
-			// copy children (Header and Body)
-			int srcchild=Node.getFirstChild(httpResponse);
-			while (srcchild!=0) {
-				// Find the equivalent node in the cordysResponse 
-				int destchild=NomUtil.getElement(cordysResponse, Node.getNamespaceURI(srcchild), Node.getLocalName(srcchild));
-				if (destchild==0) { 
-					// Node did not exist (should not happen)
-					destchild=Node.createElement(Node.getLocalName(httpResponse), cordysResponse);
-				}
-				if (Node.getLocalName(srcchild).equals("Body"))
-					NomUtil.clearNode(destchild); // Body needs boilerplate response child removed
-				NomUtil.copyAttributes(srcchild, destchild);
-				Node.duplicateAndAppendToChildren(Node.getFirstChild(srcchild), Node.getLastChild(srcchild), destchild );
-				srcchild=Node.getNextSibling(srcchild);
-			}
+
+			SoapUtil.mergeResponses(httpResponse, cordysResponse);
 	    }
 	    catch (XMLException e) { throw new RuntimeException(e); }
 	    finally {
@@ -73,7 +47,7 @@ public class HttpRelayStep extends HttpBaseStep implements Step {
 		//if (to==0)
 		//	throw new RuntimeException("Missing wsa:To element");
 		int refpar=Node.createElement("ReferenceParameters", header);
-		NomUtil.setNamespace(refpar, wsaNamespace, "wsa", false);
+		NomUtil.setNamespace(refpar, SoapUtil.wsaNamespace, "wsa", false);
 		int cb=Node.createElement("HttpConnectorCallback", refpar);
 		NomUtil.setNamespace(cb, "kisst.org", "kisst", false);
 		moveNode(header, "ReferenceParameters", cb);
@@ -82,7 +56,7 @@ public class HttpRelayStep extends HttpBaseStep implements Step {
 	}
 
 	private void moveNode(int header, String name, int dest) {
-		int orig = NomUtil.getElement(header, wsaNamespace, name);
+		int orig = NomUtil.getElement(header, SoapUtil.wsaNamespace, name);
 		if (orig==0)
 			return;
 		Node.unlink(orig);
--- src/java/org/kisst/cordys/util/SoapUtil.java
+++ src/java/org/kisst/cordys/util/SoapUtil.java
@@ -7,6 +7,9 @@ public class SoapUtil {
 	public static final String SoapNamespace="http://schemas.xmlsoap.org/soap/envelope/";
 	// public static final String SoapNamespace ="http://www.w3.org/2001/12/soap-envelope";
 
+	public static final String wsaNamespace="http://www.w3.org/2005/08/addressing";
+	//public static final String wsaAnonymous="http://www.w3.org/2005/08/addressing/anonymous";
+	
 	public static boolean isSoapFault(int node) {
 		if ("Envelope".equals(Node.getLocalName(node)))
 			node=getContent(node);
@@ -21,5 +24,33 @@ public class SoapUtil {
 		int body=NomUtil.getElement(envelope, SoapNamespace, "Body");
 		return Node.getFirstChild(body);  
 	}
+
+	/** This function merges the response from some kind of call with the boilerplate response that Cordys made. 
+	 * The boilerplate response Body should be cleared, but the Header has some important Cordys info.
+	 * The merging is a bit tricky, because of possible namespace prefix differences.
+	 */
+	public static void mergeResponses(int originalResponse, int cordysResponse) {
+		cordysResponse=Node.getParent(cordysResponse); // get Soap:Body
+		cordysResponse=Node.getParent(cordysResponse); // get Soap:Envelope
+		
+		// copy Envelope attributes
+		NomUtil.copyAttributes(originalResponse, cordysResponse);
+		// copy children (Header and Body)
+		int srcchild=Node.getFirstChild(originalResponse);
+		while (srcchild!=0) {
+			// Find the equivalent node in the cordysResponse 
+			int destchild=NomUtil.getElement(cordysResponse, Node.getNamespaceURI(srcchild), Node.getLocalName(srcchild));
+			if (destchild==0) { 
+				// Node did not exist (should not happen)
+				destchild=Node.createElement(Node.getLocalName(originalResponse), cordysResponse);
+			}
+			if (Node.getLocalName(srcchild).equals("Body"))
+				NomUtil.clearNode(destchild); // Body needs boilerplate response child removed
+			NomUtil.copyAttributes(srcchild, destchild);
+			Node.duplicateAndAppendToChildren(Node.getFirstChild(srcchild), Node.getLastChild(srcchild), destchild );
+			srcchild=Node.getNextSibling(srcchild);
+		}
+	}
+	
 
 }
