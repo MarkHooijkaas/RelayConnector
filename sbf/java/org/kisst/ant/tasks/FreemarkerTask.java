184337c26a8b1d2e9ce68c01ca5a6cb0cf06705b
Mark Hooijkaas <mark-git@hooijkaas.net>
2009-03-25 01:27:32 +0100

    First working version of Freemarker ant task

--- sbf/java/org/kisst/ant/tasks/FreemarkerTask.java
+++ sbf/java/org/kisst/ant/tasks/FreemarkerTask.java
@@ -4,12 +4,12 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.Vector;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
+import freemarker.log.Logger;
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
 import freemarker.template.SimpleHash;
@@ -17,25 +17,33 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 public class FreemarkerTask extends Task {
+	public static class VarDef {
+		public String name;
+		public String value;
+		public void setName(String name) { this.name = name; }
+		public void setValue(String value) { this.value = value; }
+	}
 
 	private String outputFile;
 	private String templateFile;
-
+    private final Vector<VarDef> vars= new Vector<VarDef>();
+    
 	// The method executing the task
 	public void execute() {
+		try {
+			Logger.selectLoggerLibrary(Logger.LIBRARY_NONE);
+		} catch (ClassNotFoundException e1) {/* ignore, should not happen, since no library is selected */	}
+		System.out.println(System.getProperty("user.dir"));
 		Configuration cfg = new Configuration();
 		try {
 			cfg.setDirectoryForTemplateLoading(new File(templateFile).getParentFile());
 			cfg.setObjectWrapper(new DefaultObjectWrapper());
 
-			Template temp = cfg.getTemplate(templateFile);
+			Template temp = cfg.getTemplate(new File(templateFile).getName());
 			
 			SimpleHash data = new SimpleHash();
-			Iterator it = this.getProject().getProperties().entrySet().iterator();
-			while (it.hasNext()) {
-				Map.Entry pairs = (Map.Entry)it.next();
-				data.put(((String)pairs.getKey()).replaceAll("[.]", "_"),pairs.getValue());
-			}			
+			for(VarDef v: vars)
+				data.put(v.name,v.value);
 
 			Writer out = new FileWriter(outputFile);
 			temp.process(data, out);
@@ -51,5 +59,11 @@ public class FreemarkerTask extends Task {
 
 	public void setTemplate(String templateFile) {
 		this.templateFile = templateFile;
+	}
+	
+	public VarDef createVar() {
+		VarDef v=new VarDef();
+		vars.add(v);
+		return v;
 	}
 }
