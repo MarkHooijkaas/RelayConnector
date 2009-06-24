e8c055d85e85757a0eea5c870e68761ec1a0972a
Mark Hooijkaas <mark-git@hooijkaas.net>
2009-06-25 01:50:21 +0200

    added create authurser, orguser and role methods

--- sbf/java/org/kisst/cordys/sbf/Sbf.java
+++ sbf/java/org/kisst/cordys/sbf/Sbf.java
@@ -10,6 +10,7 @@ import java.util.Properties;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.NTCredentials;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.PostMethod;
@@ -17,10 +18,14 @@ import org.apache.commons.httpclient.methods.StringRequestEntity;
 
 public class Sbf {
 	private static HttpClient client = new HttpClient();
-	private static String organization;
+	private static String dn;
+	private static String orgdn;
+	private static String org;
 	private static String url;
 	private static String username;
 	private static String password;
+	private static String ntlmhost;
+	private static String ntlmdomain;
 	private static Properties properties=new Properties();
 
 	public static String cordysCall(String data) {
@@ -31,20 +36,20 @@ public class Sbf {
 		PostMethod method=new PostMethod(url);
 		method.setDoAuthentication(true);
 		int statusCode;
+		String response;
 		try {
 			//HttpState state=new HttpState();
 			//state.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
 			method.setRequestEntity(new StringRequestEntity(data, "text/xml", "UTF-8"));
 			statusCode = client.executeMethod(method);
+			response=method.getResponseBodyAsString();
 		}
 		catch (HttpException e) { throw new RuntimeException(e);}
 		catch (IOException e) { throw new RuntimeException(e);}
 		if (statusCode != HttpStatus.SC_OK) {
-			throw new RuntimeException("Method failed: " + method.getStatusLine());
+			throw new RuntimeException("Method failed: " + method.getStatusLine()+"\n"+response);
 		}
-		try {
-			return method.getResponseBodyAsString();
-		} catch (IOException e) { throw new RuntimeException(e);}
+		return response;
 	}
 
 	private static String loadTemplate(String name) {
@@ -58,9 +63,39 @@ public class Sbf {
 		} catch (IOException e) { throw new RuntimeException(e); }
 		return result;
 	}
+
+	public static void createCustomRole(String org, String name ) {
+		String template=loadTemplate("CreateCustomRole.template");
+		template = template.replaceAll("\\$\\{dn}",dn);
+		template = template.replaceAll("\\$\\{name}",name);
+		template = template.replaceAll("\\$\\{org}",org);
+		System.out.println(cordysCall(template));
+	}
+
+	public static void createAuthUser(String login, String name, String fullname) {
+		String template=loadTemplate("CreateAuthUser.template");
+		template = template.replaceAll("\\$\\{dn}",dn);
+		template = template.replaceAll("\\$\\{name}",name);
+		template = template.replaceAll("\\$\\{fullname}",fullname);
+		template = template.replaceAll("\\$\\{login}",login);
+		System.out.println(cordysCall(template));
+	}
+
+	public static void createOrgUser(String org, String authname, String name, String fullname, String role) {
+		String template=loadTemplate("CreateOrgUser.template");
+		template = template.replaceAll("\\$\\{dn}",dn);
+		template = template.replaceAll("\\$\\{org}",org);
+		template = template.replaceAll("\\$\\{name}",name);
+		template = template.replaceAll("\\$\\{authname}",authname);
+		template = template.replaceAll("\\$\\{fullname}",fullname);
+		template = template.replaceAll("\\$\\{role}",role);
+		System.out.println(cordysCall(template));
+	}
+
+	
 	public static void createMethodSet(String conntype, String name, String namespace) {
 		String template=loadTemplate("CreateMethodSet.template");
-		template = template.replaceAll("\\$\\{org}",organization);
+		template = template.replaceAll("\\$\\{org}",orgdn);
 		template = template.replaceAll("\\$\\{name}",name);
 		template = template.replaceAll("\\$\\{namespace\\}",namespace);
 		template = template.replaceAll("\\$\\{type\\}",conntype);
@@ -68,7 +103,7 @@ public class Sbf {
 	}
 	public static void deleteMethodSet(String conntype, String name, String namespace) {
 		String template=loadTemplate("DeleteMethodSet.template");
-		template = template.replaceAll("\\$\\{org}",organization);
+		template = template.replaceAll("\\$\\{org}",orgdn);
 		template = template.replaceAll("\\$\\{name}",name);
 		template = template.replaceAll("\\$\\{namespace\\}",namespace);
 		template = template.replaceAll("\\$\\{type\\}",conntype);
@@ -77,7 +112,7 @@ public class Sbf {
 	}	
 	public static void createMethod(String methodset, String name, String impl, String wsdl) {
 		String template=loadTemplate("CreateMethod.template");
-		template = template.replaceAll("\\$\\{org}",organization);
+		template = template.replaceAll("\\$\\{org}",orgdn);
 		template = template.replaceAll("\\$\\{methodset}",methodset);
 		template = template.replaceAll("\\$\\{name\\}",name);
 		template = template.replaceAll("\\$\\{impl\\}",xmlEscape(impl));
@@ -105,17 +140,25 @@ public class Sbf {
 			}
 			catch (java.io.IOException e) { throw new RuntimeException(e);  }
 		}
+		dn      =(String) properties.get("cordys.dn");
+		org =(String) properties.get("cordys.organization");
+		orgdn= "o="+org+","+dn;
+
 		url      =(String) properties.get("cordys.gateway.url");
 		username=(String) properties.get("cordys.gateway.username");
 		password=(String) properties.get("cordys.gateway.password");
-		organization =(String) properties.get("cordys.organization");
+		ntlmhost=(String) properties.get("cordys.gateway.ntlmhost");
+		ntlmdomain=(String) properties.get("cordys.gateway.ntlmdomain");
 		//url="http://"+host+":"+port+"/cordys/com.eibus.web.soap.Gateway.wcp?organization="+organization;
 		if (url.indexOf("Gateway.wcp")<=0) {
 			if (! url.endsWith("/"))
 				url+="/";
-			url+="com.eibus.web.soap.Gateway.wcp?organization="+organization;
+			url+="com.eibus.web.soap.Gateway.wcp?organization="+orgdn;
 		}
-		client.getState().setCredentials(AuthScope.ANY,	new UsernamePasswordCredentials(username, password));
+		if (ntlmdomain==null)
+			client.getState().setCredentials(AuthScope.ANY,	new UsernamePasswordCredentials(username, password));
+		else
+			client.getState().setCredentials(AuthScope.ANY,	new NTCredentials(username, password, ntlmhost, ntlmdomain));
 
 	}
 }
