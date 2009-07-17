7c7694c78cf5a7ac906762c8a93e2667ca3f0f59
Mark Hooijkaas <mark-git@hooijkaas.net>
2009-07-17 08:07:21 +0200

    made the faut code in a fault step also an expression

--- src/java/org/kisst/cordys/script/commands/FaultStep.java
+++ src/java/org/kisst/cordys/script/commands/FaultStep.java
@@ -10,19 +10,20 @@ import org.kisst.cordys.script.expression.ExpressionParser;
 import com.eibus.xml.nom.Node;
 
 public class FaultStep implements Step {
-	private final String code;
+	private final Expression codeExpression;
 	private final Expression messageExpression;
 	
 	
 	public FaultStep(CompilationContext compiler, final int node) {
-		code = Node.getAttribute(node, "code");
+		codeExpression = ExpressionParser.parse(compiler, Node.getAttribute(node, "code"));
 		messageExpression = ExpressionParser.parse(compiler, Node.getAttribute(node, "message"));
 	}
 
 	public void executeStep(ExecutionContext context) {
-		String str=messageExpression.getString(context);
+		String msg=messageExpression.getString(context);
+		String code=codeExpression.getString(context);
 		if (context.debugTraceEnabled())
-			context.traceDebug("throwing SoapFaulException "+code+": "+str);
-		throw new SoapFaultException(code, str);
+			context.traceDebug("throwing SoapFaulException "+code+": "+msg);
+		throw new SoapFaultException(code, msg);
 	}
 }
