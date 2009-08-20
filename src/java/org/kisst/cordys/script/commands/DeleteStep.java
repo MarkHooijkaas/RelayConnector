3292619aa8f53424a0d2240344c0ddab7c3df522
Mark Hooijkaas <mark-git@hooijkaas.net>
2009-08-20 07:45:27 +0200

    made delete step work with repeating elements

--- src/java/org/kisst/cordys/script/commands/DeleteStep.java
+++ src/java/org/kisst/cordys/script/commands/DeleteStep.java
@@ -1,24 +1,26 @@
 package org.kisst.cordys.script.commands;
 
+import java.util.List;
+
 import org.kisst.cordys.script.CompilationContext;
 import org.kisst.cordys.script.ExecutionContext;
 import org.kisst.cordys.script.Step;
 import org.kisst.cordys.script.expression.XmlExpression;
+import org.kisst.cordys.util.NomNode;
 
 import com.eibus.xml.nom.Node;
 
 public class DeleteStep implements Step {
-    private final XmlExpression expr;
-    
+	private final XmlExpression expr;
+
 	public DeleteStep(CompilationContext compiler, final int node) {
 		String exprStr=Node.getAttribute(node,	"node");
 		expr=new XmlExpression(compiler, exprStr);
 	}
 
 	public void executeStep(final ExecutionContext context) {
-		int node=expr.getNode(context);
-		if (context.debugTraceEnabled())
-			context.traceDebug("deleting xml node "+Node.writeToString(node, false));
-		Node.delete(node);
+		List<NomNode> nodeList=expr.getNodeList(context);
+		for (NomNode n: nodeList)
+			n.destroy();
 	}
 }
